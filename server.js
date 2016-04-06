b7e31cecfb18c398a4b17fcf753d7a38c349336e
Mohammad Samiul Islam <forthright48@gmail.com>
2016-04-07 03:10:10 +0600

    Add: Handlebars rendering support with Express.
    Added Handlebars as default rendering engine.

--- server.js
+++ server.js
@@ -6,8 +6,11 @@
 
     var PORT = 8080;
 
+    /*Configuration*/
+    app.set('view engine', 'hbs'); ///Support for handlebars rendering
+
     app.get ( "/", function( req, res ) {
-        res.send ( "Hello World" );
+        res.render ( "testing.hbs", { name: "Samiul" } );
     });
 
     app.listen ( PORT, function() {

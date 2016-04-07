46f29017f55dc109ead41d297349b03190bf7488
Mohammad Samiul Islam <forthright48@gmail.com>
2016-04-07 17:44:58 +0600

    Add Layout to Hbs engine
    Added a default layout to hbs engine.

--- server.js
+++ server.js
@@ -3,16 +3,28 @@
 
     var express = require ( "express" );
     var path = require ( "path");
+    var hbs = require ( "hbs" );
     var app = express();
 
     var PORT = 8080;
 
-    /*Configuration*/
+    /***********************************************************
+    Configuration
+    ***********************************************************/
+    /*App*/
     app.set('view engine', 'hbs'); ///Support for handlebars rendering
+    app.set('views', __dirname + '/views');
     app.use(express.static( path.join ( __dirname, "/public") ) ); ///Configure the public folder
+
+    /*HBS*/
+    hbs.registerPartials(__dirname + '/views/partials');
 
     app.get ( "/", function( req, res ) {
         res.render ( "home.hbs" );
+    });
+
+    app.get ( "/problem-creation", function ( req, res ) {
+        res.render ( "problem-creation/problem-creation.hbs");
     });
 
     app.listen ( PORT, function() {

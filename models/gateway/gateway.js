0c724e6c618f49ec4090b07271834c28e41d141f
Mohammad Samiul Islam <forthright48@gmail.com>
2016-04-20 20:09:45 +0600

    Add: Model for gateway

---
+++ models/gateway/gateway.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+    var mongoose = require ( "mongoose" );
+    var express = require ( "express" );
+
+    var schema = new mongoose.Schema({
+        parentId: mongoose.Schema.ObjectId,
+        ind: Number, ///To reorder items inside same folder
+        type: String, ///Folder vs item
+        name: String,
+        body: String, /// Contains text body
+        platform: String,
+        pid: String,
+        link: String, /// Link for problem or text
+        hint: String
+    });
+
+    var Gate = mongoose.model("Gateway", schema );
+
+    module.exports = {
+        model: Gate
+    };
+
+}());

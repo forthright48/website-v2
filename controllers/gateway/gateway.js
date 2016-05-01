d628c90ad4d21fdb4ac33b5c55847feb994e08ab
Mohammad Samiul Islam <forthright48@gmail.com>
2016-05-01 22:48:55 +0600

    Adds edit capabitlity and removal of items from gateway

--- controllers/gateway/gateway.js
+++ controllers/gateway/gateway.js
@@ -13,17 +13,20 @@
         res.redirect("/gateway/getChildren/000000000000000000000000");
     });
 
-    router.get("/getChildren/:parentID", getChildren );
-    router.get("/read/:ID", read );
-    router.get("/hint/:ID", hint );
+    router.get("/getChildren/:parentID", getChildren);
+    router.get("/read/:ID", read);
+    router.get("/hint/:ID", hint);
 
-    adminRouter.get( "/insert/:parentID", getInsert );
-    adminRouter.post( "/insert", postInsert );
+    adminRouter.get("/insert/:parentID", getInsert);
+    adminRouter.post("/insert", postInsert);
+    adminRouter.get("/edit/:ID", getEditItem);
+    adminRouter.post("/edit/:ID", postEditItem);
+    adminRouter.post("/delete/:ID", deleteItem);
 
     module.exports = {
         addRouter: function(app) {
             app.use("/gateway", router);
-            app.use("/admin/gateway", adminRouter );
+            app.use("/admin/gateway", adminRouter);
         }
     };
 
@@ -31,7 +34,7 @@
     Implementation
     *******************************************/
 
-    function getChildren (req, res, next) {
+    function getChildren(req, res, next) {
         var parentID = req.params.parentID;
 
         ///Find all the documents whose parentId equals to parentID
@@ -39,14 +42,14 @@
             parentId: parentID
         }).sort({
             ind: 1
-        }).exec(function(err, data ) {
+        }).exec(function(err, data) {
             if (err) return next(err);
 
             ///We also need the parentID document to create "Go Up" button
-            Gate.findOne ( {
+            Gate.findOne({
                 _id: parentID
-            }).exec ( function ( err, root ){
-                if ( err ) next ( err );
+            }).exec(function(err, root) {
+                if (err) next(err);
 
                 ///What if we are dealing with root directory that has no document
                 root = root || {
@@ -63,36 +66,36 @@
         });
     }
 
-    function read ( req, res, next ) {
+    function read(req, res, next) {
         var ID = req.params.ID;
 
-        Gate.findOne ({
+        Gate.findOne({
             _id: ID
-        }).exec ( function ( err, data ) {
-            if ( err ) return next ( err );
+        }).exec(function(err, data) {
+            if (err) return next(err);
 
-            marked ( data.body, function ( err, content ) {
-                if ( err ) return next ( err );
+            marked(data.body, function(err, content) {
+                if (err) return next(err);
                 data.body = content;
-                return world.myRender( req, res, "gateway/read", {
+                return world.myRender(req, res, "gateway/read", {
                     data: data
                 });
             });
         });
     }
 
-    function hint ( req, res, next ) {
+    function hint(req, res, next) {
         var ID = req.params.ID;
 
-        Gate.findOne ({
+        Gate.findOne({
             _id: ID
-        }).exec ( function ( err, data ) {
-            if ( err ) return next ( err );
+        }).exec(function(err, data) {
+            if (err) return next(err);
 
-            marked ( data.hint, function ( err, content ) {
-                if ( err ) return next ( err );
+            marked(data.hint, function(err, content) {
+                if (err) return next(err);
                 data.body = content;
-                return world.myRender( req, res, "gateway/read", {
+                return world.myRender(req, res, "gateway/read", {
                     data: data
                 });
             });
@@ -103,38 +106,68 @@
     Admin Router
     *******************************************/
 
-    function getInsert ( req, res ) {
-        var parentID = req.params.parentID;
-        return world.myRender ( req, res, "gateway/insert", { parentID: parentID } );
-    }
-
-    function postInsert ( req, res ) {
-
-        var content = syncModel ( req.body );
-
-        var newItem = new Gate( content );
-
-        newItem.save ( function ( err, data ) {
-            if ( err ) return next ( err );
-
-            res.redirect ( "/gateway/getChildren/" + data.parentId );
+    function getInsert(req, res) {
+        var parentId = req.params.parentID;
+        return world.myRender(req, res, "gateway/insert", {
+            parentId: parentId
         });
     }
 
-    function syncModel ( data ) {
-        var res = {};
+    function postInsert(req, res) {
+        var content = {};
+        syncModel(content, req.body);
+        var newItem = new Gate(content);
+        newItem.save(function(err, data) {
+            if (err) return next(err);
+            return res.redirect("/gateway/getChildren/" + data.parentId);
+        });
+    }
+
+    function getEditItem(req, res) {
+        var ID = req.params.ID;
+        Gate.findById(ID, function(err, data) {
+            if (err) return next(err);
+            return world.myRender(req, res, "gateway/edit-item", data);
+        });
+    }
+
+    function postEditItem(req, res) {
+        var ID = req.params.ID;
+        Gate.findById(ID, function(err, data) {
+            if (err) return next(err);
+            syncModel(data, req.body);
+
+            data.save(function(err) {
+                if (err) return next(err);
+                return res.redirect("/gateway/getChildren/" + data.parentId);
+            });
+        });
+    }
+
+    function deleteItem(req, res) {
+        var ID = req.params.ID;
+        Gate.findById(ID, function ( err, data) {
+            if ( err ) return next ( err );
+
+            var parentId = data.parentId;
+
+            data.remove ( function ( err ) {
+                if ( err ) return next ( err );
+                return res.redirect ( "/gateway/getChildren/" + parentId );
+            });
+        });
+    }
+
+    ///res is mutated
+    function syncModel(res, data) {
         res.type = data.type;
         res.parentId = data.parentId;
         res.ind = data.ind;
         res.name = data.name;
-
         res.body = data.body;
-
         res.platform = data.platform;
         res.pid = data.pid;
         res.link = data.link;
         res.hint = data.hint;
-
-        return res;
     }
 }());
